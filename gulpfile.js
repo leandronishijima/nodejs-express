cfc7d6f99a5879b8d10d0dcda562a724a5109672
Leandro <leandro.nishijima@gmail.com>
2017-04-20 16:47:35 -0300

    add gulpfile.js + gulp-nodemon

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+	nodemon = require('gulp-nodemon');
+
+gulp.task('default', function() {
+	nodemon({
+		script: 'src/index.js',
+		ext: 'js',
+		env: {
+			PORT: 3000
+		},
+		ignore: ['/node_modules/**']
+	});
+	.on('restart', function() {
+		console.log('Restarting');
+	});
+});
